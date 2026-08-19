22e28c13c07c8cd124277964d28baba178452395
Andrés Felipe leal Sánchez <andres.fas.070893@gmail.com>
2026-08-19 16:13:36 -0500

    Cambiar PUT por PATCH en Products

---
+++ Dtos/Products/PatchProductDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TechnicalChallenge.API.Dtos.Products;
+
+    public class PatchProductDto
+    {
+        public string? Name { get; set; }
+        public decimal? Price { get; set; }
+        public string? Category { get; set; }
+        public string? Availability { get; set; }
+        public string? Condition { get; set; }
+        public string? Brand { get; set; }
+        public string? SourceUrl { get; set; }
+    }
